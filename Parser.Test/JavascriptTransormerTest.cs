f6d78bf7e582f56a2c3a3ecc170c675822bc01b3
NITECO\long.tran2 <long.tran2@niteco.se>
2017-09-08 17:52:22 +0700

    Fix error in Unittest for Addition method

--- Parser.Test/JavascriptTransormerTest.cs
+++ Parser.Test/JavascriptTransormerTest.cs
@@ -7,10 +7,10 @@ namespace Parser.Test
     public class JavascriptTransormerTest
     {
         [Theory]
-        [InlineData("function() { return 1; }", "1")]
-        [InlineData("function() { return 1.25; }", "1.25")]
-        [InlineData("function() { return true; }", "true")]
-        [InlineData("function() { return false; }", "false")]
+        [InlineData("function(context) { return 1; }", "1")]
+        [InlineData("function(context) { return 1.25; }", "1.25")]
+        [InlineData("function(context) { return true; }", "true")]
+        [InlineData("function(context) { return false; }", "false")]
         public void Constant(string expected, string input)
         {
             var result = JavascriptTransformer.Transform(input);
@@ -18,7 +18,25 @@ namespace Parser.Test
         }
 
         [Theory]
-        [InlineData("function() { return 1 + 2; }", "1 + 2")]
+        [InlineData("function(context) { return 1 + 2; }", "1 + 2")]
+        [InlineData("function(context) { return 2 - 1; }", "2 - 1")]
+        [InlineData("function(context) { return 2 * 1; }", "2 * 1")]
+        [InlineData("function(context) { return 2 / 1; }", "2 / 1")]
+        [InlineData("function(context) { return 2 % 1; }", "2 % 1")]
+        [InlineData("function(context) { return 2 < 1; }", "2 < 1")]
+        [InlineData("function(context) { return 2 <= 1; }", "2 <= 1")]
+        [InlineData("function(context) { return 2 == 1; }", "2 = 1")]
+        [InlineData("function(context) { return 2 != 1; }", "2 != 1")]
+        [InlineData("function(context) { return 2 > 1; }", "2 > 1")]
+        [InlineData("function(context) { return 2 >= 1; }", "2 >= 1")]
+        [InlineData("function(context) { return Math.pow(2, 2); }", "2 ^ 2")]
+        [InlineData("function(context) { return String.prototype.concat.call(2, 2); }", "2 :: 2")]
+        [InlineData("function(context) { return -(1); }", "-1")]
+        [InlineData("function(context) { return !(1); }", "not 1")]
+        [InlineData("function(context) { return 2 && 1; }", "2 and 1")]
+        [InlineData("function(context) { return context.ComposeAnd(2, 1); }", "2 && 1")]
+        [InlineData("function(context) { return context.ComposeOr(2, 1); }", "2 || 1")]
+        [InlineData("function(context) { return context.Inverse(1); }", "! 1")]
         public void Addition(string expected, string input)
         {
             var result = JavascriptTransformer.Transform(input);
